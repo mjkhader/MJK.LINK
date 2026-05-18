55eb14e99e2cc8d3a7ac4748af947a3e542a8d95
Mjkhader <Majd.K.khader@gmail.com>
2026-05-18 16:58:49 +0300

    Add Swagger support and utility project with constants
    Replaced OpenAPI with Swashbuckle for Swagger UI integration and updated launch settings to open Swagger by default. Added core project references to the Web API. Introduced Link.Utility.Core project with a Constants class for shared values.

---
+++ Link.Utility.Core/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Link.Utility
+{
+    public static class Constants
+    {
+        public static string ConnectionString = "";
+        public static string Swagger = string.Empty;
+
+    }
+}
